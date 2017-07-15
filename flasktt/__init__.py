10bbce7b0ed4df14e871e973720b73fff42f07d8
Ahsan Bagwan <ahsanbagwan@gmail.com>
2017-07-15 22:49:30 +0530

    Initial commit

---
+++ flasktt/__init__.py
@@ -0,0 +1,1 @@
+from .flasktt import app
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='flasktt',
+    packages=['flasktt'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
