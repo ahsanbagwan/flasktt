b74047172c652ddf510627ba1eff21a6ad9f4975
Ahsan Bagwan <ahsanbagwan@gmail.com>
2017-07-20 23:54:11 +0530

    Add flask-admin dependencies

--- flasktt/flasktt.py
+++ flasktt/flasktt.py
@@ -4,9 +4,12 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+from flask_admin import Admin     
      
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
+
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
